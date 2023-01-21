49e658e649061e54aa10832c352b4729bc0b3da1
Ankit Lakra <ankitlakra@outlook.com>
2023-01-21 09:22:57 +0000

    feat(server): added flask example to send JSON object using jsonify

--- server.py
+++ server.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask,jsonify,request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/returnjson', methods=['GET'])
+def ReturnJSON():
+    if(request.method == 'GET'):
+        data = {
+                "Title": "Hello, World",
+                "Message": "This is a simple flask json response example",
+                }
+        return jsonify(data)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8123)
